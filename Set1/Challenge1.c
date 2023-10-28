3ce1c81845cf8878aadd8032539e0298203a0465
William Johnson <wlpj2011@gmail.com>
2023-10-28 13:12:45 -0400

    Reworked hex again and started working on base64

--- Set1/Challenge1.c
+++ Set1/Challenge1.c
@@ -1,95 +1,93 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-unsigned char* convert_hex_str_to_bytes(char *input, int length);
-char* convert_bytes_to_hex_str(unsigned char *input, int length);
+unsigned char* convert_hex_str_to_bytes(char *input);
+char* convert_bytes_to_hex_str(unsigned char *input);
 void print_hex(unsigned char *input);
 
-unsigned char* convert_base64_str_to_bytes(char *input, int length);
-char* convert_bytes_to_base64(unsigned char *input, int length);
+unsigned char* convert_base64_str_to_bytes(char *input);
+char* convert_bytes_to_base64_str(unsigned char *input);
 void print_base64(unsigned char *input);
 
+unsigned char convert_hex_char_to_int(char input);
+unsigned char convert_base64_char_to_int(char input);
 
 int main(int argc, char *argv[]){
   if(argc != 2){
     printf("Usage: %s hexinput",argv[0]);
     exit(1);
   }
-
+  printf("\n");
   printf("The input was %s.\n",argv[1]);
 
-  int length = 0;
-  while(argv[1][length] != 0x00){
-    length += 1;
-  }
-    
-  
-  
-  unsigned char *bytes = convert_hex_str_to_bytes(argv[1],length);
-  length = length/2;
-  for(int i = 0; i < length; i++){
-    printf("%d ",bytes[i]);
-  }
-  printf("\n");
-  char *newhex = convert_bytes_to_hex_str(bytes,length);
+  unsigned char *bytes = convert_hex_str_to_bytes(argv[1]);
   print_hex(bytes);
   free(bytes);
-  free(newhex);
 
-  printf("\n");
+  printf("\n\n");
   exit(0);
 }
 
-unsigned char* convert_hex_str_to_bytes(char *input, int length){
-  // Converts a string of "length" hex characters into an array of byte values
+unsigned char convert_hex_char_to_int(char input){
+  // Outputs that value of the nibble that corresponds to the input hex character, or outputs 255 in the case of an invalid hex character
+  if((input > 47) && (input < 58)){
+    return input - 48;
+  } else if((input > 96) && (input < 103)){
+    return input - 87;
+  } else if((input > 64) && (input < 71)){
+    return input - 55;
+  } else {
+    return 255;
+  }
+}
+
+unsigned char* convert_hex_str_to_bytes(char *input){
+  // Converts a string of null terminated hex characters into an array of byte values
   // Note that we will make the 0th byte of the output be the number of bytes stored, so we can only return up to 255 bytes for now.
   // #TODO: Fix this so it can hold at least 65535 bytes, maybe more.
+  int length = 0;
+  while(input[length] != 0x00){
+    length +=1;
+  }
   unsigned char *output = malloc((1+(length/2))*sizeof(char));
   output[0] = length/2;
   for(int i = 0; i < length/2; i++){
-    if((input[2*i] > 47) && (input[2*i] < 58)){
-      output[i+1] += 16*(input[2*i] - 48);
-    } else if((input[2*i] > 96) && (input[2*i] < 103)){
-      output[i+1] += 16*(input[2*i] - 87);
-    } else if((input[2*i] > 64) && (input[2*i] < 71)){
-      output[i+1] += 16*(input[2*i] - 55);
-    } else {
-      printf("Character %c in position %d is not a valid hex character.\n",input[2*i] ,2*i);
+    unsigned char temp = convert_hex_char_to_int(input[2*i]);
+    if(temp == 255){
+      printf("Character %c in position %d is not a valid hex character.\n\n",input[2*i] ,2*i);
       free(output);
       exit(1);
     }
-    if((input[2*i+1] > 47) && (input[2*i+1] < 58)){
-      output[i+1] += (input[2*i+1]-48);
-    } else if((input[2*i+1] > 96) && (input[2*i+1] < 103)){
-      output[i+1] += (input[2*i+1]-87);
-    } else if((input[2*i+1] > 64) && (input[2*i+1] < 71)){
-      output[i+1] += (input[2*i+1]-55);
-    } else {
-      printf("Character %c in position %d is not a valid hex character.\n",input[2*i+1], 2*i+1);
+    output[i+1] = (temp << 4);
+    temp = convert_hex_char_to_int(input[2*i+1]);
+    if(temp == 255){
+      printf("Character %c in position %d is not a valid hex character.\n\n",input[2*i+1], 2*i+1);
       free(output);
       exit(1);
     }
+    output[i+1] += temp;
   }
   return output;
 }
 
 
-char* convert_bytes_to_hex_str(unsigned char *input, int length){
-  // Converts a char array of "length" bytes into a hex string
+char* convert_bytes_to_hex_str(unsigned char *input){
+  // Converts a char array of bytes into a hex string
   // Note that the output here is a null terminated string with length length*2+1 including the null terminator
+  int length = input[0];
   char *output = malloc(sizeof(char)*(length*2+1));
 
   for(int i = 0; i < length; i++){
-    if(((input[i]&0xf0)>>4)>9){
-      output[2*i] = ((input[i] & 0xf0) >> 4) + 87;
+    if(((input[i+1]&0xf0)>>4)>9){
+      output[2*i] = ((input[i+1] & 0xf0) >> 4) + 87;
     } else {
-      output[2*i] = ((input[i] & 0xf0) >> 4) + 48;
+      output[2*i] = ((input[i+1] & 0xf0) >> 4) + 48;
     }
-    if((input[i]&0x0f) > 9){
-      output[2*i+1] = (input[i] & 0x0f) + 87;
+    if((input[i+1]&0x0f) > 9){
+      output[2*i+1] = (input[i+1] & 0x0f) + 87;
     }
     else {
-      output[2*i+1] = (input[i] & 0x0f) + 48;
+      output[2*i+1] = (input[i+1] & 0x0f) + 48;
 
     }
   }
@@ -98,24 +96,59 @@ char* convert_bytes_to_hex_str(unsigned char *input, int length){
 }
 
 void print_hex(unsigned char *input){
-  int length = input[0];
-  char *hex_str = convert_bytes_to_hex_str(input+1,length);
+  // Prints a array of chars storing a list of bytes as a hex string
+  char *hex_str = convert_bytes_to_hex_str(input);
   printf("0x%s",hex_str);
   free(hex_str);
   return;
 }
 
-unsigned char* convert_base64_str_to_bytes(char *input, int length){
-  unsigned char *output = malloc(sizeof(unsigned char)*length);
+unsigned char convert_base64_char_to_int(char input){
+  if((input > 64) && (input < 91)){
+    return input -65;
+  } else if((input > 96) && (input < 123)){
+    return input - 97 + 26;
+  } else if((input > 47) && (input < 58)){
+    return input - 48 + 52;
+  } else if(input == 43){
+    return 62;
+  } else if(input == 47){
+    return 63;
+  } else if(input == 61){
+    return 64;
+  } else {
+    return 255;
+  }
+}
+
+unsigned char* convert_base64_str_to_bytes(char *input){
+  // Converts a null terminated base64 string into a list of bytes where the 0th position stores the number of bytes
+  // TODO: make this so it can store 65535 bytes instead of only 255
+  int length = 0;
+  while(input[length] != 0x00){
+    length += 1;
+  }
+  unsigned char *output = malloc(sizeof(unsigned char)*(((3*length)/4)+1));
+  output[0] = length;
+
+  for(int i = 0; i < (length/4); i++){
+    
+  }
+  
   return output;
 }
-char* convert_bytes_to_base64(unsigned char *input, int length){
+
+char* convert_bytes_to_base64_str(unsigned char *input){
+  int length = input[0];
   char *output = malloc(sizeof(char)*length);
+
+  
   return output;
 }
 
-void print_base65(unsigned char *input){
-
-
+void print_base64(unsigned char *input){
+  char *base64_str = convert_bytes_to_base64_str(input);
+  printf("%s",base64_str);
+  free(base64_str);
   return;
 }
