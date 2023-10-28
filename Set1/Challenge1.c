4296dfbf389bd6297d5b733d405530d59323bddf
William Johnson <wlpj2011@gmail.com>
2023-10-28 18:27:53 -0400

    Finished base64_str_to_bytes, including padding. Reorganized some of the hex stuff again.

--- Set1/Challenge1.c
+++ Set1/Challenge1.c
@@ -10,7 +10,9 @@ char* convert_bytes_to_base64_str(unsigned char *input);
 void print_base64(unsigned char *input);
 
 unsigned char convert_hex_char_to_int(char input);
+char *convert_byte_to_hex_chars(unsigned char input);
 unsigned char convert_base64_char_to_int(char input);
+
 
 int main(int argc, char *argv[]){
   if(argc != 2){
@@ -20,7 +22,9 @@ int main(int argc, char *argv[]){
   printf("\n");
   printf("The input was %s.\n",argv[1]);
 
-  unsigned char *bytes = convert_hex_str_to_bytes(argv[1]);
+  unsigned char *bytes = convert_base64_str_to_bytes(argv[1]);
+
+  
   print_hex(bytes);
   free(bytes);
 
@@ -70,6 +74,21 @@ unsigned char* convert_hex_str_to_bytes(char *input){
   return output;
 }
 
+char *convert_byte_to_hex_chars(unsigned char input){
+  //Converts a single byte into a pair of hex characters
+  char *output = malloc(sizeof(char)*2);
+  if(((input&0xf0)>>4)>9){
+    output[0] = ((input & 0xf0)>>4)+87;
+  } else {
+    output[0] = ((input & 0xf0)>>4)+48;
+  }
+  if((input&0x0f) > 9){
+    output[1] = (input & 0x0f) + 87;
+  } else {
+    output[1] = (input & 0x0f) + 48;
+  }
+  return output;
+}
 
 char* convert_bytes_to_hex_str(unsigned char *input){
   // Converts a char array of bytes into a hex string
@@ -78,18 +97,10 @@ char* convert_bytes_to_hex_str(unsigned char *input){
   char *output = malloc(sizeof(char)*(length*2+1));
 
   for(int i = 0; i < length; i++){
-    if(((input[i+1]&0xf0)>>4)>9){
-      output[2*i] = ((input[i+1] & 0xf0) >> 4) + 87;
-    } else {
-      output[2*i] = ((input[i+1] & 0xf0) >> 4) + 48;
-    }
-    if((input[i+1]&0x0f) > 9){
-      output[2*i+1] = (input[i+1] & 0x0f) + 87;
-    }
-    else {
-      output[2*i+1] = (input[i+1] & 0x0f) + 48;
-
-    }
+    char *temp = convert_byte_to_hex_chars(input[i+1]);
+    output[2*i] = temp[0];
+    output[2*i+1] = temp[1];
+    free(temp);
   }
   output[length*2] = 0x00;
   return output;
@@ -104,6 +115,7 @@ void print_hex(unsigned char *input){
 }
 
 unsigned char convert_base64_char_to_int(char input){
+  //Converts a single base64 character into an int between 0 and 63. Returns 64 for padding character and 255 for illegal character
   if((input > 64) && (input < 91)){
     return input -65;
   } else if((input > 96) && (input < 123)){
@@ -129,16 +141,65 @@ unsigned char* convert_base64_str_to_bytes(char *input){
     length += 1;
   }
   unsigned char *output = malloc(sizeof(unsigned char)*(((3*length)/4)+1));
-  output[0] = length;
+  output[0] = (3*length)/4;
 
   for(int i = 0; i < (length/4); i++){
-    
+
+    unsigned char temp = convert_base64_char_to_int(input[4*i]);
+    int char3value = 0;
+    if(temp == 255){
+      printf("Character %c in position %d is not a valid base64 character.\n\n",input[4*i], 4*i);
+      free(output);
+      exit(1);
+    }
+    char3value += (temp << 18);
+
+    temp = convert_base64_char_to_int(input[4*i+1]);
+    if(temp == 255){
+      printf("Character %c in position %d is not a valid base64 character.\n\n",input[4*i+1], 4*i+1);
+      free(output);
+      exit(1);
+    } else if(temp == 64){
+      printf("Character %c is position %d is not a valid padding of base64.\n\n", input[4*i+1], 4*i+1);
+      free(output);
+      exit(1);
+    }
+    char3value += (temp << 12);
+
+    temp = convert_base64_char_to_int(input[4*i+2]);
+    if(temp == 255){
+      printf("Character %c in position %d is not a valid base64 character.\n\n",input[4*i+2], 4*i+2);
+      free(output);
+      exit(1);
+    } else if(temp == 64){
+      output[3*i+1] = ((char3value & 0xff0000) >> 16);
+      output[0] -= 2;
+      return output;
+    }
+    char3value += (temp << 6);
+
+    temp = convert_base64_char_to_int(input[4*i+3]);
+    if(temp == 255){
+      printf("Character %c in position %d is not a valid base64 character.\n\n",input[4*i+3], 4*i+3);
+      free(output);
+      exit(1);
+    } else if(temp == 64){
+      output[3*i+1] = ((char3value & 0xff0000) >> 16);
+      output[3*i+2] = ((char3value & 0x00ff00) >> 8);
+      output[0] -= 1;
+      return output;
+    }
+    char3value += temp;
+
+    output[3*i+1] = ((char3value & 0xff0000) >> 16);
+    output[3*i+2] = ((char3value & 0x00ff00) >> 8);
+    output[3*i+3] = (char3value & 0x0000ff);
   }
-  
   return output;
 }
 
 char* convert_bytes_to_base64_str(unsigned char *input){
+  //Converts a list of bytes into a string with its base64 representation
   int length = input[0];
   char *output = malloc(sizeof(char)*length);
 
@@ -147,6 +208,7 @@ char* convert_bytes_to_base64_str(unsigned char *input){
 }
 
 void print_base64(unsigned char *input){
+  //Prints a list of bytes as its base64 representation
   char *base64_str = convert_bytes_to_base64_str(input);
   printf("%s",base64_str);
   free(base64_str);
