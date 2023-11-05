43a1665a419369ff8708b6f251199c020e0c5d04
William Johnson <wlpj2011@gmail.com>
2023-11-05 18:37:04 -0500

    Added ascii frequency table

--- Set1/CheckDecryptUtilities.c
+++ Set1/CheckDecryptUtilities.c
@@ -1,5 +1,7 @@
 #include <stdbool.h>
 
+const float ascii_frequencies[] = {0.2254, 0.0727, 0.0133, 0.0248, 0.0381, 0.1126, 0.0195, 0.0177, 0.0541, 0.0621, 0.0013, 0.0068, 0.0355, 0.0213, 0.0594, 0.0665, 0.0169, 0.0008, 0.0532, 0.0559, 0.0807, 0.0248, 0.0087, 0.0213, 0.0013, 0.0177, 0.0007};
+
 bool byte_stream_is_ascii(unsigned char *bytes){
   int length = bytes[0];
   for(int i =0; i < length; i++){
--- Set1/CheckDecryptUtilities.h
+++ Set1/CheckDecryptUtilities.h
@@ -1,4 +1,5 @@
 #include <stdbool.h>
 
+extern const float ascii_frequencies[];
 bool byte_stream_is_ascii(unsigned char *bytes);
 int byte_stream_compare_character_distribution(unsigned char *bytes);
