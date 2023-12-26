632ad35a5937dc3ededede54f7d45be7d2c6b35e
William Johnson <wlpj2011@gmail.com>
2023-12-26 10:37:05 -0600

    Partial progress on Challenge 4, still no actual solution

--- Set1/Challenge4.c
+++ Set1/Challenge4.c
@@ -26,10 +26,43 @@ int main(int argc, char *argv[]){
   //Need a loop over the number of lines in the file
   //Want to read a single line from fptr and store it as a string hex_bytes
 
-  
-  unsigned char *enc_bytes = convert_hex_str_to_bytes(hex_bytes);
+  int LINELENGTH = 62;
+  char hex_bytes[LINELENGTH];
 
+  while(fgets(hex_bytes,sizeof hex_bytes, fptr) != NULL){
+    //printf("%s",hex_bytes);
+    if(hex_bytes[LINELENGTH] == '\n'){
+      hex_bytes[LINELENGTH] = 0x00;
+    }
+    unsigned char *enc_bytes = convert_hex_str_to_bytes(hex_bytes);
 
+    int length = enc_bytes[0];
+    float minimum_decrypt_score = 27.0;
+    int best_decrypt_key = 0x00;
+    for(int i=0; i < 256; i++){
+      unsigned char *testkey = malloc(sizeof(unsigned char) * (length + 1));
+      testkey[0] = length;
+      for(int j = 0; j < length; j++){
+	testkey[j+1] = i;
+      }
+      //      print_hex(enc_bytes);
+      unsigned char *testdecrypt = xor_byte_streams(enc_bytes,testkey);
+      if(byte_stream_is_ascii(testdecrypt)){
+	float value = byte_stream_compare_character_distribution(testdecrypt);
+	if(value < minimum_decrypt_score){
+	  minimum_decrypt_score = value;
+	  best_decrypt_key = i;
+	  printf("The key 0x%02x gives the decrypt ", i);
+	  print_ascii(testdecrypt);
+	}
+      }
+      free(testkey);
+      free(testdecrypt);
+    }
+    printf("\n");
+    free(enc_bytes);
+  }
+ 
   /*
   int length = bytes1[0];
   float minimum_decrypt_score = 27.0;
@@ -57,7 +90,7 @@ int main(int argc, char *argv[]){
     free(testdecrypt);
   }
   */
-  free(bytes1);
+  //free(bytes1);
 
   fclose(fptr);
   
