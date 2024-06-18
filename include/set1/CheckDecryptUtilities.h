f2ee86a0b6fbe99b2f32d6c196a089c68b701440
William Johnson <wlpj2011@gmail.com>
2024-06-17 20:07:31 -0400

    Minor modifications, need sorting algorithm to progress. Added sorted frequnecy nubmers

--- include/set1/CheckDecryptUtilities.h
+++ include/set1/CheckDecryptUtilities.h
@@ -2,5 +2,6 @@
 #include <stdbool.h>
 
 extern const float ascii_frequencies[];
+extern const float ascii_frequencies_sorted[];
 bool byte_stream_is_ascii(unsigned char *bytes);
 float byte_stream_compare_character_distribution(unsigned char *bytes);
--- src/set1/DiagnoseEncryptionUtilities.c
+++ src/set1/DiagnoseEncryptionUtilities.c
@@ -1,6 +1,13 @@
 #include <stdbool.h>
 
 bool byte_stream_is_1_byte_key_xor(unsigned char *bytes){
-
+  int length = bytes[0];
+  int counts[256] = {0};
+  int count = 0;
+  for(int i=0; i < length; i++){
+    counts[bytes[i+1]] += 1;
+    count += 1;
+  }
+  
   return false
 }
